46721dd510a729f0c07a54a0c6e80f8a8d4aab5a
Ryan Walker <ryandanielwalker@gmail.com>
2021-03-23 11:06:41 -0700

    Create peek a boo website
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+
+  $(".fade-in").click(function() {
+    $("#img-in").fadeToggle();
+  });
+
+  $("#img-out").click(function() {
+    $("#img-out").fadeOut();
+  });
+
+  $(".slide-up").click(function() {
+    $("#img-mondrian").slideUp();
+  });
+
+  $(".slide-down").click(function() {
+    $("#img-mondrian").slideDown();
+  });
+
+  $("#slide-toggle").click(function() {
+    $("#img-mondrian").slideToggle();
+  });
+});
